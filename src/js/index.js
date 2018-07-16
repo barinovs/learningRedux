b83e19c0b5fa41f6b7e9766a55b47784b29ba384
barinovs <barinovserge@gmail.com>
2018-07-16 23:13:46 +0300

    commit 2018-07-16 23:13

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+// src/js/Index.js
+
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./components/App";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 render(
   <Provider store={store}>
     <App />
